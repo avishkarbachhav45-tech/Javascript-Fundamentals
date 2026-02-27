b51da5443c86911ec69f843f4f7629bc1b468eb2
Avishkar Bachhav <avishkarbachhav45@gmail.com>
2026-02-27 18:20:35 +0530

    Datatypes and ECMA standards

---
+++ 01-Basics/02_Datatypes.js
@@ -0,0 +1,26 @@
+"use strict" // treat all JS code as newer version
+
+// alert (3+3) // we are using nodejs, not browser
+
+console.log( 3 
+    +
+    3) // code readability should be high
+
+let name = "Avi"
+let age = 21
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""  ''
+// boolean => true/false
+// null => standalone value
+// undefined
+//symbol => unique
+// object
+
+
+// typeof
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined

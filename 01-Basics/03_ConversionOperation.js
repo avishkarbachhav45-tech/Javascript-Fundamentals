7eae9afd110af6818e3f7538a39cd8de8fa70b95
Avishkar Bachhav <avishkarbachhav45@gmail.com>
2026-07-20 17:12:20 +0530

    Conversion Operations

---
+++ 01-Basics/03_ConversionOperation.js
@@ -0,0 +1,21 @@
+let score = "3s3" // 454, "abc", true, undefined, null
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let numberInNumber = Number(score);
+// console.log(typeof numberInNumber);
+// console.log(numberInNumber);
+
+// Boolean conversion
+let isLoggedIn = 1
+
+let checkThis = Boolean(isLoggedIn)
+// console.log(checkThis);
+
+// String conversion
+let someNumber = 45
+
+let toString = String(someNumber)
+console.log(toString);
+console.log(typeof toString);
--- 01-Basics/test.js
+++ 01-Basics/test.js
@@ -1,1 +1,10 @@
 console.log("Avishkar")
+
+let name = "Avishkar"
+const num = "54545"
+
+let accountId;
+
+accountId = "Hitman"
+
+console.table([name, num, accountId])

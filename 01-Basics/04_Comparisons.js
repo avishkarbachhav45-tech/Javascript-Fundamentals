4aeae38b19df7b1c47e88b331429b0f3b04d73a1
Avishkar Bachhav <avishkarbachhav45@gmail.com>
2026-07-21 20:05:07 +0530

    Comparisons in JavaScript

---
+++ 01-Basics/04_Comparisons.js
@@ -0,0 +1,19 @@
+console.log(2 > 1);
+console.log(2 < 1);
+console.log(2 == 2);
+console.log(2 <= 2);
+console.log(5 >= 2);
+console.log(6 != 5);
+
+console.log("2" > 1);
+console.log("02" < 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// strict check ===

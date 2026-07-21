c94ea8d08131c821bd1d10dd5323f165818f0fdf
Avishkar Bachhav <avishkarbachhav45@gmail.com>
2026-07-21 19:27:44 +0530

    Conversion Operations

--- 01-Basics/03_ConversionOperation.js
+++ 01-Basics/03_ConversionOperation.js
@@ -17,5 +17,36 @@ let checkThis = Boolean(isLoggedIn)
 let someNumber = 45
 
 let toString = String(someNumber)
-console.log(toString);
-console.log(typeof toString);
+// console.log(toString);
+// console.log(typeof toString);
+
+
+// Operations
+
+let value = 3;
+let negValue = -value
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**5);
+// console.log(2/2);
+// console.log(2%5);
+
+let str1 = "Hello"
+let str2 = " Avi"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1" + 8);
+// console.log(8 + "1");
+// console.log("8" + 1 + 1);
+// console.log(1 + 1 + "8");
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);

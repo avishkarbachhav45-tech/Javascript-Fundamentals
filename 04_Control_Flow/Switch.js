c53b45544b89f245dc2012da39ad5d77f07ed456
Avishkar Bachhav <avishkarbachhav45@gmail.com>
2026-08-06 17:20:15 +0530

    Switch case in JS

---
+++ 04_Control_Flow/Switch.js
@@ -0,0 +1,43 @@
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("Jan");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 2:
+        console.log("Apr");
+        break;
+
+    default:
+        console.log("Default case checked");
+        break;
+}
+
+
+
+const months = "Mar"
+
+switch (months) {
+    case "Jan":
+        console.log("January");
+        break;
+    case "Feb":
+        console.log("February");
+        break;
+    case "Mar":
+        console.log("March");
+        break;
+    case "Apr":
+        console.log("April");
+        break;
+
+    default:
+        console.log("Default case checked");
+        break;
+}

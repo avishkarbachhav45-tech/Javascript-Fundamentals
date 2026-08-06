efafa78841761d0798833e06020b9335535aea41
Avishkar Bachhav <avishkarbachhav45@gmail.com>
2026-08-06 17:07:23 +0530

    If else condition in JS

---
+++ 04_Control_Flow/one.js
@@ -0,0 +1,49 @@
+// const temprature = 42
+
+// if(temprature === 41){
+//     console.log(`Temprature is below 50`);
+// }
+// else{
+//     console.log(`Temprature is above 50`);
+// }
+
+// console.log("Execute");
+//  <, >, <=, >=, ==, !=, ===
+
+// const score = 300
+
+// if(score > 150){
+//     let power = "Fly"
+//     console.log(`Power : ${power}`)
+// }
+// console.log(`Power : ${power}`);
+
+const balance = 1000
+
+// if(balance > 500) console.log("Test");
+
+// if(balance < 500){
+//     console.log("Less than 500");
+// }
+// else if(balance < 750){
+//     console.log("Less than 750");
+// }
+// else if(balance < 900){
+//     console.log("Less than 900");
+// }
+// else{
+//     console.log("Greater than 1200");
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromGmail = true
+
+if(userLoggedIn && debitCard && 25==25){
+    console.log("Allow to buy course");
+}
+
+if(loggedInFromGmail || loggedInFromGoogle){
+    console.log("User logged in");
+}

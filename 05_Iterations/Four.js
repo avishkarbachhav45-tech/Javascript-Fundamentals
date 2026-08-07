13936b4d2039bcb10f66fd66549aaa7bd2ff9571
Avishkar Bachhav <avishkarbachhav45@gmail.com>
2026-08-07 22:58:53 +0530

    for in loop in JS

---
+++ 05_Iterations/Four.js
@@ -0,0 +1,25 @@
+const myObj = {
+    py : "Python",
+    js : "JavaScript",
+    cpp : "c++",
+    rb : "ruby"
+}
+
+for (const key in myObj) {
+    // console.log(`${key} is shortcut of - ${myObj[key]}`);
+}
+
+const myArr = ["JS", "PY", "CPP"]
+for (const num in myArr) {
+    // console.log(`Value is ${myArr[num]}`);   
+}
+
+// const map = new Map()
+
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// console.log(map);
+

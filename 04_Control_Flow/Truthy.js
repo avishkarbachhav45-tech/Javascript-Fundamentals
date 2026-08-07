5fb1bcc1597c19e5044af788e0fa06b329060796
Avishkar Bachhav <avishkarbachhav45@gmail.com>
2026-08-07 10:23:10 +0530

    nullish coalescing and ternary operator

--- 04_Control_Flow/Truthy.js
+++ 04_Control_Flow/Truthy.js
@@ -1,9 +1,9 @@
 const userEmail = []
 
 if(userEmail){
-    console.log("Got user email");
+    // console.log("Got user email");
 } else{
-    console.log("Don't have email");
+    // console.log("Don't have email");
 }
 
 // Truthy Values
@@ -14,8 +14,30 @@ if(userEmail){
 
 // false, 0, "", BigInt 0n, null, undefined, NaN
 
-if(userEmail.length == 1){
-    console.log("Valid Email");
-} else{
-    console.log("Invalid Email");
+// if(userEmail.length == 1){
+//     console.log("Valid Email");
+// } else{
+//     console.log("Invalid Email");
+// }
+
+const emptyObject = {}
+
+if(Object.keys(emptyObject).length === 0){
+    // console.log("Object is empty");
 }
+
+// Nullish Coalescing operator (??) : null, undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 10
+val1 = null ?? 10 ?? 20
+
+// console.log(val1);
+
+// Ternary operator
+
+const teaPrice = 100
+
+teaPrice <= 80 ?  console.log("less than 80") :console.log("greater than 80")
